8e64a16a2c1a987ed006e9acf6fd377d0c18199a
Cristóbal Gazali <cjgazali@uc.cl>
2019-06-10 11:39:28 -0400

    movies show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    # unuse this
-    @response = RestClient.get('https://swapi.co/api/films/')
-    @movies = JSON.parse(@response)["results"]
-
-    body = {"query": "query{allFilms{edges{node{title,releaseDate,episodeID,director,producers,id}}}}"}
+    body = {"query": "query{allFilms{edges{node{title,releaseDate,episodeID,director,producers}}}}"}
     response = query_request(body)
     @movies = response["allFilms"]["edges"]
   end
@@ -12,5 +8,17 @@ class MoviesController < ApplicationController
   def show
     response = RestClient.get('https://swapi.co/api/films/' + params[:id] + '/')
     @movie = JSON.parse(response)
+
+    id = params[:id].to_i
+    if [1, 2, 3].include? id
+      id += 3
+    elsif [4, 5, 6].include? id
+      id -= 3
+    end
+    puts id
+
+    body = {"query": "query { film(filmID: #{id}) { episodeID, title, openingCrawl, director, producers, releaseDate, characterConnection { characters { name, id } }, planetConnection { planets { name, id } }, starshipConnection { starships { name, id } } } }"}
+    response = query_request(body)
+    @movie = response["film"]
   end
 end
