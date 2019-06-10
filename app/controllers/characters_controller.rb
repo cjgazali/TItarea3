d8e3dd20489a78cfc1e2025e977e4283e1127ea5
Cristóbal Gazali <cjgazali@uc.cl>
2019-06-10 12:15:40 -0400

    show character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,7 @@
 class CharactersController < ApplicationController
   def show
-    response = RestClient.get('https://swapi.co/api/people/' + params[:id] + '/')
-    @character = JSON.parse(response)
+    body = {"query": "query { person(id: \"#{params[:id]}\") { height, mass, hairColor, skinColor, eyeColor, birthYear, gender, homeworld { name, id }, filmConnection { films { title, episodeID } }, starshipConnection { starships { name, id } } } }"}
+    response = query_request(body)
+    @character = response["person"]
   end
 end
