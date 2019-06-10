52c038be4252edb74569a7f190829dbe5de00c75
Cristóbal Gazali <cjgazali@uc.cl>
2019-06-10 12:56:32 -0400

    show starship and fixes None at planet

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -1,6 +1,7 @@
 class StarshipsController < ApplicationController
   def show
-    response = RestClient.get('https://swapi.co/api/starships/' + params[:id] + '/')
-    @starship = JSON.parse(response)
+    body = {"query": "query { starship(id: \"#{params[:id]}\") { name, model, manufacturers, costInCredits, length, maxAtmospheringSpeed, crew, passengers, cargoCapacity, consumables, hyperdriveRating, MGLT, starshipClass, filmConnection { films { title, episodeID } }, pilotConnection { pilots { name, id } } } }"}
+    response = query_request(body)
+    @starship = response["starship"]
   end
 end
